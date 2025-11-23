a033ad20bf14873d65283a645ceee7f23f99280a
Amy Liu <liu.amy05@gmail.com>
2025-11-23 18:40:28 -0500

    ci: add VisualStudioTools plugin to project and relevant source files
    [git-p4: depot-paths = "//Cadence/K8_Main/": change = 70]

--- Source/Cadence/Gesture/AGestureRecorder.cpp
+++ Source/Cadence/Gesture/AGestureRecorder.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Gesture/AGestureRecorder.h"
+#include "AGestureRecorder.h"
 
 // Sets default values
 AAGestureRecorder::AAGestureRecorder()
